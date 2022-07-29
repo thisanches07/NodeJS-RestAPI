812b57f860c4f0d74f682e6596b5bb15ad2e0a67
Thiago_Sanches <thi.sanches@hotmail.com>
2022-07-29 11:17:23 -0300

    relacionamento entre post e usuário

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -46,21 +47,29 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: "Thiago Sanches",
-    },
+    creator: req.userId,
   });
 
   post
     .save()
     .then((result) => {
+      User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "POST criado com sucesso!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
