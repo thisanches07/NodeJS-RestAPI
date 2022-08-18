41af63ef2779e2bd5ce0c0c420d18a19f5eba3c4
Thiago Sanches <thi.sanches@hotmail.com>
2022-08-18 14:28:41 -0300

    update and delete post - graphQl

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const fs = require("fs");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +10,7 @@ const { graphqlHTTP } = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 const auth = require("./middleware/auth");
+const { clearImage } = require("./util/file");
 
 const app = express();
 
@@ -116,8 +116,3 @@ mongoose
   )
   .then((result) => app.listen(8080))
   .catch((err) => console.log(err));
-
-const clearImage = (filePath) => {
-  filePath = path.join(__dirname, "..", filePath);
-  fs.unlink(filePath, (err) => console.log(err));
-};
